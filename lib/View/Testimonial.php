99f530dcf41f1f846b63337305682453d17e459b
sinharakesh <rksinha.btech@gmail.com>
2018-09-03 01:56:51 +0530

    testimonila slider adeed

--- lib/View/Testimonial.php
+++ lib/View/Testimonial.php
@@ -3,6 +3,7 @@
 namespace xavoc\dictionary;
 
 class View_Testimonial extends \CompleteLister{
+   public $count=0;
    function init(){
       parent::init();
       
@@ -13,18 +14,15 @@ class View_Testimonial extends \CompleteLister{
       $this->setModel($model);
    }
    
-   // function formatRow(){
+   function formatRow(){
+      if($this->count == 1)
+         $this->current_row_html['active'] = "active";
+      else
+         $this->current_row_html['active'] = "deactive";
+      $this->count++;
 
-      // if($this->model['page_name']){
-      //    if($this->model['slug_url']){
-      //       $this->current_row_html['url'] = $this->app->url($this->model['page_name'],['slug'=>$this['slug_url']]);
-      //    }else
-      //       $this->current_row_html['url'] = $this->app->url($this->model['page_name']);  
-      // }else
-      //    $this->current_row_html['url'] = "";
-
-      // parent::formatRow();
-   // }
+      parent::formatRow();
+   }
 
    function defaultTemplate(){
       return ['view/testimonial'];
